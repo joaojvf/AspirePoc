e17ee32604dc068868548c27aae52f96f8f3a5ab
joaojvf <joaovictorf02@hotmail.com>
2024-03-10 17:26:01 -0300

    feat: Add SQL Server with EFCore

--- src/AspirePoc.Aspire.AppHost/Program.cs
+++ src/AspirePoc.Aspire.AppHost/Program.cs
@@ -2,8 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var rabbitMQ = builder
     .AddRabbitMQContainer("rabbitmq-aspire", port: 5672, password: "guest");
+var db1 = builder.AddSqlServer("sql-aspire").AddDatabase("libraryDb");
 
 builder.AddProject<Projects.AspirePoc_UI_Api>("aspirepoc.ui.api")
-        .WithReference(rabbitMQ);
+        .WithReference(rabbitMQ)
+        .WithReference(db1);
 
 builder.Build().Run();
--- src/AspirePoc.UI.Api/Program.cs
+++ src/AspirePoc.UI.Api/Program.cs
@@ -2,6 +2,8 @@ using AspirePoc.Infrastructure.SqlServer;
 using AspirePoc.Core;
 using AspirePoc.Infrastructure.RabbitMQ;
 using Microsoft.EntityFrameworkCore;
+using AspirePoc.Core.Abstractions.Repositories;
+using AspirePoc.Infrastructure.SqlServer.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.SqlDataProjectSetup(builder.Configuration);
+SetupInfrastructure();
 builder.Services.CoreProjectSetup();
 builder.Services.RabbitMQSetup(builder.Configuration);
 
@@ -33,3 +35,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+
+void SetupInfrastructure()
+{
+    builder.AddSqlServerDbContext<ApplicationContext>("libraryDb");
+    builder.Services.AddScoped<IBookRepository, BookRepository>();
+    builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+}
