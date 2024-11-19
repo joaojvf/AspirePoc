33249fa74b1c9c4a5c9dd96ad3ef1bea104242f3
joaojvf <joaovictorf02@hotmail.com>
2024-11-19 08:30:47 -0300

    feat: add policy retry to rabbitmq subscribe

--- src/AspirePoc.Infrastructure.RabbitMQ/RabbitMQMessageBus.cs
+++ src/AspirePoc.Infrastructure.RabbitMQ/RabbitMQMessageBus.cs
@@ -1,22 +1,39 @@
 ﻿using AspirePoc.Core.Abstractions;
 using EasyNetQ;
+using Polly;
+using Polly.Retry;
 
 namespace AspirePoc.Infrastructure.RabbitMQ
 {
     internal class RabbitMQMessageBus : IMessageBus
     {
         private IBus _bus;
+        private readonly AsyncRetryPolicy _retryPolicy;
+
 
         public RabbitMQMessageBus(string connectionString)
         {
             _bus = RabbitHutch.CreateBus(connectionString);
+            _retryPolicy = Policy
+                .Handle<EasyNetQException>()
+                .Or<TimeoutException>()
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    onRetry: (exception, timeSpan, retryCount, context) =>
+                    {
+                        Console.WriteLine($"Retry {retryCount} for SubscribeAsync due to: {exception.Message}");
+                    });
 
         }
         public bool IsConnected => _bus?.Advanced.IsConnected ?? false;
 
-        public void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
+        public async void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
         {
-            _bus.PubSub.Subscribe(subscriptionId, onMessage);
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                await _bus.PubSub.SubscribeAsync(subscriptionId, onMessage);
+            });
         }
         public void PublishMessage<T>(T message, string routingKey)
         {
